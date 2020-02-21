221bee56a68436b6d3e456e41e610ca3eed29f62
Amanda Sarenana <59600132+AmandaSarenana@users.noreply.github.com>
2020-02-20 23:56:26 -0800

    asarenana1@student.rccd.edu

---
+++ Homework/Hello World/Hello World/main.cpp
@@ -0,0 +1,41 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Amanda Sarenana
+ * Created on February 20, 2020 11:46PM
+ * Purpose: Hello World
+ */
+
+//System Level Libraries
+#include <iostream>   //I/O Library
+using namespace std;  //Libraries compiled under std
+
+//User Level Libraries
+
+//Global Constants - Science/Math Related
+//Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    //Variable Declarations
+    
+    //Variable Initialization
+    
+    //Mapping Process Inputs to Outputs
+    
+    //Display Outputs
+    cout<<"Hello World"<<endl;//All in 1 Line
+    cout<<"Hello World";//All in 2 Lines
+    cout<<endl;
+    cout<<"Hello ";//All in 3 Lines
+    cout<<"World";
+    cout<<endl;
+
+    //Clean Up
+    
+    //Exit stage right!
+    return 0;
+}
